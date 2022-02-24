ae26c75a17bc62f73dcda0fb659a572a3cf78dca
aj235711 <amirjaved68504@gmail.com>
2022-02-24 19:41:02 +0530

    done hehe

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,7 +1,76 @@
-const Todos = () => {
-    return (
-        <div>Todos Page</div>
-    )
-}
+import { useState, useEffect } from "react";
 
-export default Todos
+import { BiAddToQueue } from "react-icons/bi";
+
+import Todo from "./Todo";
+
+const Todos = ({ user }) => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  useEffect(() => {
+    const getTodos = async () => {
+      try {
+        const res = await fetch(
+          `https://todo-api-production.up.railway.app/getAll?userId=${user._id}`
+        );
+        const data = await res.json();
+        if (!data.success) {
+          window.alert("Failed fetching Todos");
+        }
+        setTodos(data.todos);
+      } catch (error) {
+        console.log(error);
+        window.alert("Server error");
+      }
+    };
+    getTodos();
+  }, []);
+
+  const addTodo = async () => {
+    try {
+      const res = await fetch(
+        "https://todo-api-production.up.railway.app/create",
+        {
+          method: "post",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ userId: user._id, description: newTodo }),
+        }
+      );
+      const data = await res.json();
+      if (!data.success) {
+        window.alert("Failed adding Todo");
+      }
+      setTodos(data.newTodos);
+      setNewTodo("");
+    } catch (error) {
+      console.log(error);
+      window.alert("Server error");
+    }
+  };
+
+  return (
+    <div className="todoContainer">
+      <div className="newTodoWrapper">
+        <input
+          name="newTodo"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          placeholder="Add Item"
+          className="newTodo"
+        />
+        <button className="addBtn" onClick={addTodo}>
+          <BiAddToQueue className="addIcon" />
+          Add
+        </button>
+      </div>
+      {todos.map((todo) => (
+        <Todo todo={todo} user={user} setTodos={setTodos} />
+      ))}
+    </div>
+  );
+};
+
+export default Todos;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import { UserProvider } from './context/userContext'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <UserProvider>
-      <App />
-    </UserProvider>
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
